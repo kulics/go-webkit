d605cc7eb407ccdd1fcd960fc79a812566029c26
Kulics <kulics@xylaga.com>
2019-03-30 23:56:51 +0800

    增加文件上传方法

--- web_client.go
+++ web_client.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -43,6 +45,22 @@ func (sf *WebClient) HTTPRequest(method Method, relativePath string,
 	return sf.cli.Do(req)
 }
 
+func (sf *WebClient) processRequest(method Method, relativePath string,
+	contentType string, params io.Reader, handles ...responseHandle) ([]byte, error) {
+	resp, err := sf.HTTPRequest(method, relativePath, ContentTypeForm, params)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	for _, v := range handles {
+		err = v(*resp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return ioutil.ReadAll(resp.Body)
+}
+
 // mapToParams 将map转换为参数
 func mapToParams(params Form) (io.Reader, error) {
 	data := make([]string, 0)
@@ -63,69 +81,47 @@ func mapToParams(params Form) (io.Reader, error) {
 
 // FormRequest 表单请求
 func (sf *WebClient) FormRequest(method Method, relativePath string, params Form,
-	responseHanlde ...responseHandle) ([]byte, error) {
+	handles ...responseHandle) ([]byte, error) {
 	reader, err := mapToParams(params)
 	if err != nil {
 		return nil, err
 	}
-	resp, err := sf.HTTPRequest(method, relativePath, ContentTypeForm, reader)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	for _, v := range responseHanlde {
-		err = v(*resp)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return ioutil.ReadAll(resp.Body)
+	return sf.processRequest(method, relativePath, ContentTypeForm, reader, handles...)
 }
 
 // FormGET 表单get
 func (sf *WebClient) FormGET(relativePath string, params Form,
-	responseHanlde ...responseHandle) ([]byte, error) {
-	return sf.FormRequest(GET, relativePath, params)
+	handles ...responseHandle) ([]byte, error) {
+	return sf.FormRequest(GET, relativePath, params, handles...)
 }
 
 // FormPOST 表单post
 func (sf *WebClient) FormPOST(relativePath string, params Form,
-	responseHanlde ...responseHandle) ([]byte, error) {
-	return sf.FormRequest(POST, relativePath, params)
+	handles ...responseHandle) ([]byte, error) {
+	return sf.FormRequest(POST, relativePath, params, handles...)
 }
 
 // FormPUT 表单put
 func (sf *WebClient) FormPUT(relativePath string, params Form,
-	responseHanlde ...responseHandle) ([]byte, error) {
-	return sf.FormRequest(PUT, relativePath, params)
+	handles ...responseHandle) ([]byte, error) {
+	return sf.FormRequest(PUT, relativePath, params, handles...)
 }
 
 // FormDELETE 表单delete
 func (sf *WebClient) FormDELETE(relativePath string, params Form,
-	responseHanlde ...responseHandle) ([]byte, error) {
-	return sf.FormRequest(DELETE, relativePath, params)
+	handles ...responseHandle) ([]byte, error) {
+	return sf.FormRequest(DELETE, relativePath, params, handles...)
 }
 
 // JSONRequest JSON请求
 func (sf *WebClient) JSONRequest(method Method, relativePath string,
 	params interface{}, response interface{},
-	responseHanlde ...responseHandle) error {
+	handles ...responseHandle) error {
 	bt, err := json.Marshal(params)
 	if err != nil {
 		return err
 	}
-	resp, err := sf.HTTPRequest(method, relativePath, ContentTypeJSON, bytes.NewReader(bt))
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	for _, v := range responseHanlde {
-		err = v(*resp)
-		if err != nil {
-			return err
-		}
-	}
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := sf.processRequest(method, relativePath, ContentTypeJSON, bytes.NewReader(bt), handles...)
 	if err != nil {
 		return err
 	}
@@ -135,27 +131,65 @@ func (sf *WebClient) JSONRequest(method Method, relativePath string,
 // JSONGET JSON get
 func (sf *WebClient) JSONGET(relativePath string,
 	params interface{}, response interface{},
-	responseHanlde ...responseHandle) error {
-	return sf.JSONRequest(GET, relativePath, params, response)
+	handles ...responseHandle) error {
+	return sf.JSONRequest(GET, relativePath, params, response, handles...)
 }
 
 // JSONPOST JSON post
 func (sf *WebClient) JSONPOST(relativePath string,
 	params interface{}, response interface{},
-	responseHanlde ...responseHandle) error {
-	return sf.JSONRequest(POST, relativePath, params, response)
+	handles ...responseHandle) error {
+	return sf.JSONRequest(POST, relativePath, params, response, handles...)
 }
 
 // JSONPUT JSON put
 func (sf *WebClient) JSONPUT(relativePath string,
 	params interface{}, response interface{},
-	responseHanlde ...responseHandle) error {
-	return sf.JSONRequest(PUT, relativePath, params, response)
+	handles ...responseHandle) error {
+	return sf.JSONRequest(PUT, relativePath, params, response, handles...)
 }
 
 // JSONDELETE JSON delete
 func (sf *WebClient) JSONDELETE(relativePath string,
 	params interface{}, response interface{},
-	responseHanlde ...responseHandle) error {
-	return sf.JSONRequest(DELETE, relativePath, params, response)
+	handles ...responseHandle) error {
+	return sf.JSONRequest(DELETE, relativePath, params, response, handles...)
+}
+
+// FileInfo 发送文件类型
+type FileInfo struct {
+	Field  string
+	Path   string
+	Params map[string]string
+}
+
+// FileUpload 文件上传方法
+func (sf *WebClient) FileUpload(relativePath, field, path string, params map[string]string,
+	handles ...responseHandle) ([]byte, error) {
+	fileBuffer := &bytes.Buffer{}
+	bodyWriter := multipart.NewWriter(fileBuffer)
+	fileWriter, err := bodyWriter.CreateFormFile(field, path)
+	if err != nil {
+		return nil, err
+	}
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	_, err = io.Copy(fileWriter, f)
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range params {
+		err = bodyWriter.WriteField(k, v)
+		if err != nil {
+			return nil, err
+		}
+	}
+	bodyWriter.Close()
+
+	return sf.processRequest(POST, relativePath, bodyWriter.FormDataContentType(),
+		fileBuffer, handles...)
 }
